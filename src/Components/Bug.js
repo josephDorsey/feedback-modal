83b15d383d8a943376606130eb27a11eade630e7
josephDorsey <jmdorsey92@gmail.com>
2023-06-04 10:07:28 -0700

    Add message and severity state
    It also will include the onChange handlers for both states on their respective elements

--- src/Components/Bug.js
+++ src/Components/Bug.js
@@ -1,10 +1,20 @@
 import { ReturnToMainNav } from "./ReturnToMainNav";
 import { CloseFeedbackModal } from "./CloseFeedbackModal";
 import { SendFeedbackButton } from "./SendFeedbackButton";
+import { useState } from "react";
 import bugIcon from "../assets/bug.svg";
 export function Bug(props) {
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("Low");
   const handleSubmit = () => {
     console.log(`Clicked`);
+  };
+  const handleTextChange = (e) => {
+    setMessage(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleSeverityChange = (e) => {
+    setSeverity(e.target.value);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -16,11 +26,16 @@ export function Bug(props) {
         </div>
         <CloseFeedbackModal setOpenFBModal={props.setOpenFBModal} />
       </div>
-      <textarea className="bug-text" placeholder="I noticed that..." />
+      <textarea
+        value={message}
+        onChange={handleTextChange}
+        className="bug-text"
+        placeholder="I noticed that..."
+      />
       <div className="bug-labels-container">
         <div className="severity-container">
           <label>Severity: </label>
-          <select>
+          <select value={severity} onChange={handleSeverityChange}>
             <option>Low</option>
             <option>Medium</option>
             <option>High</option>
