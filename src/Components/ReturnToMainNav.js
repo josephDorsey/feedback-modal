25e277fbbc869ae893683ffe9ead768c955c6842
josephDorsey <jmdorsey92@gmail.com>
2023-06-04 13:01:14 -0700

    feat: Add conditionals to toggle return

--- src/Components/ReturnToMainNav.js
+++ src/Components/ReturnToMainNav.js
@@ -2,8 +2,15 @@ import backButton from "../assets/arrow-back.svg";
 
 export function ReturnToMainNav(props) {
   const handleReturnToMainNav = () => {
-    props.setBug(false);
-    // props.setIdea(false);
+    if (props.bug) {
+      props.setBug(false);
+    }
+    if (props.idea) {
+      props.setIdea(false);
+    }
+    if (props.other) {
+      props.setOther(false);
+    }
     // props.setOther(false);
     props.setMainNav(true);
   };
